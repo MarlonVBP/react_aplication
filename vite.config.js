357d11a422e1fc695796992332e4d0ff119e40b8
MarlonVBP <marlonvicctor13@gmail.com>
2024-10-07 14:50:03 -0300

    #

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://marlonvbp.github.io/react_aplication",
+  base: "/react_aplication",
   plugins: [react()],
 })
 
